023bf35e0cc4c687f18937282ebb1384cda9ec50
Towsifa Akhter <towsifa.akhter@gmail.com>
2024-11-27 17:07:43 +0100

    adding trigger paths

--- AnaProd/HH_bbtautau/anaTupleDef.py
+++ AnaProd/HH_bbtautau/anaTupleDef.py
@@ -72,7 +72,7 @@ FatJetObservablesMC = ["hadronFlavour","partonFlavour"]
 SubJetObservables = ["btagDeepB", "eta", "mass", "phi", "pt", "rawFactor"]
 SubJetObservablesMC = ["hadronFlavour","partonFlavour"]
 
-defaultColToSave = ["entryIndex","luminosityBlock", "run","event", "sample_type", "sample_name", "period", "X_mass", "X_spin", "isData","PuppiMET_pt", "PuppiMET_phi", "nJet","DeepMETResolutionTune_pt", "DeepMETResolutionTune_phi","DeepMETResponseTune_pt", "DeepMETResponseTune_phi","PV_npvs"]
+defaultColToSave = ["entryIndex","luminosityBlock", "run","event", "sample_type", "sample_name", "period", "X_mass", "X_spin", "isData","PuppiMET_pt", "PuppiMET_phi", "nJet","DeepMETResolutionTune_pt", "DeepMETResolutionTune_phi","DeepMETResponseTune_pt", "DeepMETResponseTune_phi","PV_npvs", "Rho_fixedGridRhoAll"]
 
 def getDefaultColumnsToSave(isData):
     colToSave = defaultColToSave.copy()
--- Analysis/scripts/make_stackplots.py
+++ Analysis/scripts/make_stackplots.py
@@ -1,20 +1,16 @@
 import os
 
-
-indir = "/eos/user/d/daebi/ANA_FOLDER_DEV/histograms/Run32022_13Nov24/Run3_2022/merged/"
-
-varnames = ["lep1_pt", "lep1_eta", "lep2_pt", "lep2_eta", "bjet1_btagPNetB", "bjet2_btagPNetB"]
-varnames = ["lep1_pt"]
-channellist = ["e", "eE", "eMu", "mu", "muMu"]
-channellist = ["mu", "muMu"]
-
 era = "Run3_2022"
-plotdir = "plots_19Nov_NoMuonSF/"
+plotdir = "plots/"
+indir = f"/eos/user/t/toakhter/HH_bbtautau_Run3/histograms/HLT/{era}/merged/"
+
+varnames = ["PV_npvs", "Rho_fixedGridRhoAll", "tau1_pt", "tau2_pt"]#["lep1_pt", "lep1_eta", "lep2_pt", "lep2_eta", "bjet1_btagPNetB", "bjet2_btagPNetB"]
+channellist = ["eE", "eMu", "muMu", "eTau", "muTau", "tauTau"]
 
 for var in varnames:
     for channel in channellist:
-        filename = os.path.join(indir, var, f"{var}.root")
+        filename = os.path.join(indir, var, f"tmp/all_histograms_{var}_Central.root")
         print("Loading fname ", filename)
         os.makedirs(plotdir, exist_ok=True)
-        outname = os.path.join(plotdir, f"HHbbWW_{channel}_{var}_StackPlot.pdf")
-        os.system(f"python3 ../HistPlotter.py --inFile {filename} --bckgConfig ../../config/HH_bbWW/background_samples.yaml --globalConfig ../../config/HH_bbWW/global.yaml --outFile {outname} --var {var} --category inclusive --channel {channel} --uncSource Central --wantData True --year Run3_2022 --wantQCD False --rebin False --analysis HH_bbWW --qcdregion OS_Iso --sigConfig ../../config/HH_bbWW/{era}/samples.yaml")
+        outname = os.path.join(plotdir, f"HHbbtautau_{channel}_{var}_StackPlot.pdf")
+        os.system(f"python3 HistPlotter.py --inFile {filename} --bckgConfig ../../config/HH_bbtautau/{era}/background_samples.yaml --globalConfig ../../config/HH_bbtautau/global.yaml --sigConfig ../../config/{era}/samples.yaml --outFile {outname} --var {var} --category inclusive --channel {channel} --uncSource Central --wantData True --year {era} --wantQCD False --rebin False --analysis HH_bbtautau --qcdregion OS_Iso")
